1f09df5d2b72431af0b8aca382e3f90d1ec7a5a0
KBryan <kwame@3einteractive.com>
2015-04-04 10:22:32 -0400

    Initial Commit

---
+++ FlickrApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrApp
+//
+//  Created by Kwame Bryan on 2015-04-04.
+//  Copyright (c) 2015 3e Interactive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
